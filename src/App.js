91ef3337bd2e1a6d2791b20fd238cce7d091d7a7
Deepika590 <deepikas984319@gmail.com>
2025-10-06 20:04:53 +0530

    First React project commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Crud from './CrudApp/CrudApp';
+function App() {
+  return (
+    <div className="App">
+      <Crud />
+    </div>
+  );
+}
+
+export default App;
